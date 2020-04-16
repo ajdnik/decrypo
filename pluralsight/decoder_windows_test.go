a121ecc9de88d42cfa12bb9539905439274a0345
Rok Ajdnik <r.ajdnik@gmail.com>
2020-04-16 16:07:24 +0200

    chore: expanded windows decoder tests

--- pluralsight/decoder_windows_test.go
+++ pluralsight/decoder_windows_test.go
@@ -2,6 +2,7 @@ package pluralsight_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/ajdnik/decrypo/pluralsight"
@@ -16,7 +17,7 @@ var decoderTests = []struct {
 	{"key v2", []byte{2, 51, 126, 235, 210, 234, 244, 147, 197, 218, 156, 152, 214, 246, 54, 84}, []byte{0, 0, 0, 36, 102, 116, 121, 112, 109, 112, 52, 50, 0, 0, 0, 0}},
 }
 
-func TestDecoder_Decode(t *testing.T) {
+func TestDecoder_DecodeBuffer(t *testing.T) {
 	decoder := pluralsight.Decoder{}
 	for _, tt := range decoderTests {
 		t.Run(tt.desc, func(t *testing.T) {
@@ -38,6 +39,29 @@ func TestDecoder_Decode(t *testing.T) {
 
 			if !bytes.Equal(tt.out, out) {
 				t.Errorf("got %v, want %v", out, tt.out)
+			}
+		})
+	}
+}
+
+var decoderErrorTests = []struct {
+	desc string
+	in   []byte
+	out  error
+}{
+	{"no error", []byte{118, 82, 13, 52, 161, 175, 137, 129, 110, 235, 198, 107, 182, 86, 144, 120}, nil},
+	{"unknown stream", []byte{1, 2, 3, 4, 5}, pluralsight.ErrUnknownStream},
+	{"undefined buffer", nil, io.EOF},
+}
+
+func TestDecoder_DecodeError(t *testing.T) {
+	decoder := pluralsight.Decoder{}
+	for _, tt := range decoderErrorTests {
+		t.Run(tt.desc, func(t *testing.T) {
+			r := bytes.NewReader(tt.in)
+			_, err := decoder.Decode(r)
+			if err != tt.out {
+				t.Errorf("got %v, want %v", err, tt.out)
 			}
 		})
 	}
